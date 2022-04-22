a10ee69f1fca9636f3890854908ef8c97209c920
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-22 23:47:08 +0800

    fix: 🐛 [components/useJobs.tsx] convert mock-positions.json
    make every field type is string

--- components/useJobs.tsx
+++ components/useJobs.tsx
@@ -2,6 +2,15 @@ import { useState, useEffect } from "react";
 import positions from './mock-positions.json';
 import Job from '../types/job';
 
+// positions 字段值保证为字符串
+const ps = positions.map((p: any) => {
+  let ret:any = {};
+  Object.keys(p).forEach((key)=>{
+    ret[key] = typeof p[key] === 'string' ? p[key] : ''
+  })
+  return ret;
+})
+
 type Props = {
   keyword?: string,
   id?: string
@@ -24,7 +33,7 @@ export function useJobs({ keyword, id }: Props) {
         setError('');
         // const res = await fetch(url);
         // const json = await res.json();
-        const json = positions.slice(0, 5);
+        const json = ps.slice(0, 5);
         setJobs(json);
       } catch (error) {
         setError("Failed to fetch");
