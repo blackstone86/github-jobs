fc6ebce8fb0f6e98bfd3123400ed55df6d736186
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-23 20:44:55 +0800

    fix: 🐛 [server/index.ts] Update
    convert to typescript

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+import Koa from 'koa';
+import Router from 'koa-router';
 const router = new Router();
 const app = new Koa();
-const positions = require('./mock-positions.json');
+import positions from './mock-positions.json' assert { type: "json" };
 
 // 对原数据进行清洗
 // 1. 将公司 logo 从 https://jobs.github.com/xx 切换到 https://dummyimage.com/100x100
 // 2. 对字段类型非 string 的转为空串
-const ps = positions.map((p) => {
-  let ret = {};
+const ps = positions.map((p: any) => {
+  let ret:any = {};
   Object.keys(p).forEach((key)=>{
     if(key === 'company_logo') {
       ret[key] = 'https://dummyimage.com/100x100';
@@ -21,17 +21,17 @@ const ps = positions.map((p) => {
 
 const port = 5000;
 
-router.get('/job/:id', async (ctx) => {
+router.get('/job/:id', async (ctx: any) => {
 	const { id } = ctx.params; 
-	const job = ps.filter((p)=>{
+	const job = ps.filter((p: any)=>{
 		return id === p.id;
 	})
 	ctx.body = job;
 })
 
-router.get('/job', async (ctx) => {
+router.get('/job', async (ctx: any) => {
 	const keyword = ctx.request.query.search;
-	const jobs = ps.filter(({title})=>{
+	const jobs = ps.filter(({title}: any)=>{
 		return title.toLocaleLowerCase().includes(keyword.toLowerCase());
 	})
 	ctx.body = jobs;
