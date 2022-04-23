555fd45daf2cfc98d715a1675ea510560317ae1f
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-23 16:17:11 +0800

    feat: 🎸 [next.config.js] Update next.config.js
    add rewrites for cross domain in dev mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() { 
+    return [ 
+      { 
+        source: '/apis/:path*', 
+        destination: `http://127.0.0.1:5000/:path*`
+      }, 
+    ]
+  },
 }
 
 module.exports = nextConfig
